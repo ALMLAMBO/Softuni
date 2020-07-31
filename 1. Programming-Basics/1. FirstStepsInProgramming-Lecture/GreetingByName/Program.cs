0e7de5713b950c0a37608691ee68a6a649237a73
ALMLAMBO <aleksm007148@gmail.com>
2020-07-31 15:22:53 +0300

    Add problem solution

--- 1. Programming-Basics/1. FirstStepsInProgramming-Lecture/GreetingByName/Program.cs
+++ 1. Programming-Basics/1. FirstStepsInProgramming-Lecture/GreetingByName/Program.cs
@@ -3,7 +3,8 @@
 namespace GreetingByName {
 	class Program {
 		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+			string name = Console.ReadLine();
+			Console.WriteLine($"Hello, {name}!");
 		}
 	}
 }
