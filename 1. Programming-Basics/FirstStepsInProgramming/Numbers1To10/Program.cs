230312d0044647db680dd4862f3f829bbad7d984
ALMLAMBO <aleksm007148@gmail.com>
2020-07-31 14:50:36 +0300

    Add two ways for solution

--- 1. Programming-Basics/FirstStepsInProgramming/Numbers1To10/Program.cs
+++ 1. Programming-Basics/FirstStepsInProgramming/Numbers1To10/Program.cs
@@ -3,7 +3,21 @@
 namespace Numbers1To10 {
 	class Program {
 		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+			Console.WriteLine(1);
+			Console.WriteLine(2);
+			Console.WriteLine(3);
+			Console.WriteLine(4);
+			Console.WriteLine(5);
+			Console.WriteLine(6);
+			Console.WriteLine(7);
+			Console.WriteLine(8);
+			Console.WriteLine(9);
+			Console.WriteLine(10);
+
+			//Second way
+			for (int i = 1; i <= 10; i++) {
+				Console.WriteLine(i);
+			}
 		}
 	}
 }
