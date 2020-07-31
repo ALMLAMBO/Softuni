1c72ca97f2afabe112c44ce0d0957aa1631ff264
ALMLAMBO <aleksm007148@gmail.com>
2020-07-31 15:31:16 +0300

    Add problem solution

--- 1. Programming-Basics/1. FirstStepsInProgramming-Lecture/ConcatenateData/Program.cs
+++ 1. Programming-Basics/1. FirstStepsInProgramming-Lecture/ConcatenateData/Program.cs
@@ -3,7 +3,12 @@
 namespace ConcatenateData {
 	class Program {
 		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+			string firstName = Console.ReadLine();
+			string lastName = Console.ReadLine();
+			int age = int.Parse(Console.ReadLine());
+			string town = Console.ReadLine();
+
+			Console.WriteLine($"You are {firstName} {lastName}, a {age}-years old person from {town}.");
 		}
 	}
 }
