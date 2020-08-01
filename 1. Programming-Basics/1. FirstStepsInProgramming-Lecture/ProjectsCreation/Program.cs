3fc1bb2fd08b21e51a07345af112b3609a8e8431
ALMLAMBO <aleksm007148@gmail.com>
2020-08-01 12:04:40 +0300

    Add problem solution

--- 1. Programming-Basics/1. FirstStepsInProgramming-Lecture/ProjectsCreation/Program.cs
+++ 1. Programming-Basics/1. FirstStepsInProgramming-Lecture/ProjectsCreation/Program.cs
@@ -3,7 +3,12 @@
 namespace ProjectsCreation {
 	class Program {
 		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+			string name = Console.ReadLine();
+			int projects = int.Parse(Console.ReadLine());
+
+			Console.WriteLine(
+				"The architect {0} will need {1} hours to complete {2} project/s.", 
+				name, projects * 3, projects);
 		}
 	}
 }
